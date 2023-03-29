0bd57cce73efda468e013b926d858462ff4c5a06
DGHeadHA <1097803124@qq.com>
2023-03-30 00:29:58 +0900

    更新

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # query_web
-python+flask+sqlalchemy+javascript
-
-整体技术路线为后端：python+sqlalchemy，前端：js，框架flask。
+python+flask+sqlalchemy+javascript+C++
--- database_config.py
+++ database_config.py
@@ -1,8 +1,8 @@
 USERNAME = "root"
-PASSWORD = "root"  #每个人设置的名字和账号会不同，这里是自己设定的账号密码
+PASSWORD = "root"
 HOST = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'dot_view'  #这里是数据库文件名
+DATABASE = 'dot_view'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOST,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
